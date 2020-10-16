8d415dbd5d16651bef853fa1753d640de883c04f
minhtintang <minhtintang@gmail.com>
2020-10-16 07:02:44 +0700

    Winform Database

---
+++ WinForm_Database/WinForm_Database/QuanLy.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WinForm_Database
+{
+    partial class QuanLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIDMH = new System.Windows.Forms.TextBox();
+            this.gvMonHoc = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtMH = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.numNumber = new System.Windows.Forms.NumericUpDown();
+            this.cbLyThuyet = new System.Windows.Forms.CheckBox();
+            this.cbThucHanh = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cbHocKy = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMonHoc)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIDMH);
+            this.groupBox1.Location = new System.Drawing.Point(563, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IDMH";
+            // 
+            // txtIDMH
+            // 
+            this.txtIDMH.Location = new System.Drawing.Point(6, 19);
+            this.txtIDMH.Name = "txtIDMH";
+            this.txtIDMH.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtIDMH.Size = new System.Drawing.Size(213, 20);
+            this.txtIDMH.TabIndex = 0;
+            // 
+            // gvMonHoc
+            // 
+            this.gvMonHoc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMonHoc.Location = new System.Drawing.Point(12, 12);
+            this.gvMonHoc.Name = "gvMonHoc";
+            this.gvMonHoc.Size = new System.Drawing.Size(545, 414);
+            this.gvMonHoc.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbHocKy);
+            this.groupBox2.Location = new System.Drawing.Point(563, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(225, 53);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ma_HK";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtMH);
+            this.groupBox3.Location = new System.Drawing.Point(563, 131);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox3.Size = new System.Drawing.Size(225, 53);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ten_MH";
+            // 
+            // txtMH
+            // 
+            this.txtMH.Location = new System.Drawing.Point(6, 19);
+            this.txtMH.Name = "txtMH";
+            this.txtMH.Size = new System.Drawing.Size(213, 20);
+            this.txtMH.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.numNumber);
+            this.groupBox4.Location = new System.Drawing.Point(563, 190);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox4.Size = new System.Drawing.Size(225, 53);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "So_TC";
+            // 
+            // numNumber
+            // 
+            this.numNumber.Location = new System.Drawing.Point(6, 19);
+            this.numNumber.Name = "numNumber";
+            this.numNumber.Size = new System.Drawing.Size(213, 20);
+            this.numNumber.TabIndex = 0;
+            // 
+            // cbLyThuyet
+            // 
+            this.cbLyThuyet.AutoSize = true;
+            this.cbLyThuyet.Location = new System.Drawing.Point(569, 249);
+            this.cbLyThuyet.Name = "cbLyThuyet";
+            this.cbLyThuyet.Size = new System.Drawing.Size(73, 17);
+            this.cbLyThuyet.TabIndex = 5;
+            this.cbLyThuyet.Text = "Ly Thuyet";
+            this.cbLyThuyet.UseVisualStyleBackColor = true;
+            // 
+            // cbThucHanh
+            // 
+            this.cbThucHanh.AutoSize = true;
+            this.cbThucHanh.Location = new System.Drawing.Point(702, 249);
+            this.cbThucHanh.Name = "cbThucHanh";
+            this.cbThucHanh.Size = new System.Drawing.Size(80, 17);
+            this.cbThucHanh.TabIndex = 6;
+            this.cbThucHanh.Text = "Thuc Hanh";
+            this.cbThucHanh.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(569, 340);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 35);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(690, 340);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(92, 35);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(569, 391);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(92, 35);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(690, 391);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(92, 35);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cbHocKy
+            // 
+            this.cbHocKy.FormattingEnabled = true;
+            this.cbHocKy.Location = new System.Drawing.Point(6, 19);
+            this.cbHocKy.Name = "cbHocKy";
+            this.cbHocKy.Size = new System.Drawing.Size(211, 21);
+            this.cbHocKy.TabIndex = 0;
+            // 
+            // QuanLy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 438);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbThucHanh);
+            this.Controls.Add(this.cbLyThuyet);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gvMonHoc);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "QuanLy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quan Ly Sinh Vien";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMonHoc)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView gvMonHoc;
+        private System.Windows.Forms.TextBox txtIDMH;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtMH;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown numNumber;
+        private System.Windows.Forms.CheckBox cbLyThuyet;
+        private System.Windows.Forms.CheckBox cbThucHanh;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cbHocKy;
+    }
+}
+
---
+++ WinForm_Database/WinForm_Database/QuanLy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Database
+{
+    public partial class QuanLy : Form
+    {
+        SqlConnection conn;
+        public QuanLy()
+        {
+            InitializeComponent();
+            String connectionString = ConfigurationManager.ConnectionStrings["PH1"].ConnectionString.ToString();
+            conn = new SqlConnection(connectionString);
+            show();
+        }
+
+        public void show()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM MonHoc", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            conn.Close();
+            gvMonHoc.DataSource = dt;
+            gvMonHoc.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gvMonHoc.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        public void getHocKy()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM HocKy", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            conn.Close();
+            cbHocKy.ValueMember = "Ma_HK";
+            cbHocKy.DisplayMember = "Hoc Ky E";
+
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO MonHoc VALUES (@IDMH, @Ma_HK, @TenMH, @SoTC, @LyThuyet, @ThucHanh", conn);
+            cmd.Parameters.AddWithValue("@IDMH", txtIDMH.Text);
+            cmd.Parameters.AddWithValue("@Ma_HK", cbHocKy.SelectedValue);
+            cmd.Parameters.AddWithValue("@TenMH", txtMH.Text);
+            cmd.Parameters.AddWithValue("@SoTC", numNumber.Value);
+            cmd.Parameters.AddWithValue("@LyThuyet", cbLyThuyet.CheckState == CheckState.Checked ? 1 : 0);
+            cmd.Parameters.AddWithValue("@ThucHanh", cbThucHanh.CheckState == CheckState.Checked ? 1 : 0);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Insert success");
+            show();
+        }
+    }
+}
