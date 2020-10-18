b10fceefaa210ac321fb3908b3f539cfbb26bac6
Minh Tin Tang <minhtintang@gmail.com>
2020-10-18 19:09:52 +0700

    BabyStore

---
+++ BabyStore/BabyStore/DAL/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using BabyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace BabyStore.DAL
+{
+    public class StoreContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
